af190dbc44d731b551d1fd0321dfa060a008c209
Ivo Gosemann <ivo.gosemann@gmail.com>
2025-04-23 21:51:30 +0200

    update splaytoraid keymap

--- keyboards/0xcb/splaytoraid/keymaps/ivogoman/keymap.c
+++ keyboards/0xcb/splaytoraid/keymaps/ivogoman/keymap.c
@@ -16,9 +16,9 @@ enum layer_names {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_40(
-              KC_Q,         KC_W,         KC_E,         KC_R,         KC_T,               KC_Y,     KC_U,             KC_I,             KC_O,             KC_P,
-    KC_TAB,  LCTL_T(KC_A), LALT_T(KC_S), LSFT_T(KC_D), LGUI_T(KC_F),  KC_G,               KC_H,     RGUI_T(KC_J),     RSFT_T(KC_K),     LALT_T(KC_L),     RCTL_T(KC_SCLN),  KC_BSPC,
-    KC_LCTL,   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,              KC_N,     KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,          KC_QUOTE,
+              KC_Q,         KC_W,         KC_F,         KC_P,         KC_G,               KC_J,     KC_L,             KC_U,             KC_Y,             KC_SCLN,
+    KC_TAB,  LCTL_T(KC_A), LALT_T(KC_R), LSFT_T(KC_S), LGUI_T(KC_T),  KC_D,               KC_H,     RGUI_T(KC_N),     RSFT_T(KC_E),     LALT_T(KC_I),     RCTL_T(KC_O),  KC_BSPC,
+    KC_LCTL,   KC_Z,         KC_X,         KC_C,         KC_V,         KC_B,              KC_K,     KC_M,             KC_COMM,          KC_DOT,           KC_SLSH,       KC_QUOTE,
                                   KC_UNDS,  LTNAV_ESC,   KC_ENT,                          LTNUM_SPC,   LTSYM_BSPC,    KC_DEL
   ),
 
@@ -31,8 +31,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_SYM] = LAYOUT_40(
               KC_GRV ,  KC_LABK,  KC_RABK,  KC_MINS,  KC_PIPE,            KC_CIRC,  KC_LCBR,  KC_RCBR,  KC_DLR , _______,
-    KC_BSLS,  KC_EXLM,  KC_ASTR,  KC_SLSH,  KC_EQL,  KC_AMPR,            KC_HASH,  KC_LPRN,  KC_RPRN,  KC_SCLN, KC_DQUO,  _______,
-    _______,  KC_TILD,  KC_PLUS,  KC_LBRC,  KC_RBRC,  KC_PERC,            KC_AT  ,  KC_COLN,  KC_COMM,  KC_DOT , KC_QUOT,  _______,
+    KC_BSLS,  KC_EXLM,  KC_ASTR,  KC_SLSH,  KC_EQL,  KC_AMPR,             KC_HASH,  KC_LPRN,  KC_RPRN,  KC_SCLN, KC_DQUO,  _______,
+    _______,  KC_TILD,  KC_PLUS,  KC_LBRC,  KC_RBRC,  KC_PERC,            KC_AT  ,  KC_COLON,  KC_COMM,  KC_DOT , KC_QUOT,  _______,
                                   _______,  _______,  _______,            _______,  _______,  _______
   ),
 
@@ -50,7 +50,7 @@ const char chordal_hold_layout[MATRIX_ROWS][MATRIX_COLS] PROGMEM =
              'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
         'L', 'L', 'L', 'L', 'L', 'L',  'R', 'R', 'R', 'R', 'R', 'R',
-                       'L', 'L', 'L',  'R', '*', 'R'
+                       '*', '*', '*',  '*', '*', '*'
     );
 
 const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
